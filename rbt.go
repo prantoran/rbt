1e62ab70d9823678862fb0ab3e03613d3e79561e
prantoran <prantoran@gmail.com>
2018-07-22 16:15:45 +0600

    insert fixup

--- rbt.go
+++ rbt.go
@@ -78,5 +78,47 @@ func (t *Tree) Insert(i Item) {
 }
 
 func (t *Tree) insertFixup(z *node) {
-
+	var y *node
+	for z.parent != nil && z.parent.c == Red {
+		// root must always be black, hence if z.parent is red,
+		// then z.parent.parent exists
+		if z.parent == z.parent.parent.left {
+			y = z.parent.parent.right // uncle
+			if y.c == Red {
+				z.parent.c = Black
+				y.c = Black
+				z.parent.parent.c = Red
+				z = z.parent.parent
+				continue
+			}
+			if z == z.parent.right {
+				z = z.parent
+				t.leftRotate(z)
+			}
+			z.parent.c = Black
+			z.parent.parent.c = Red
+			t.rightRotate(z.parent.parent)
+			continue
+		}
+		// symmetrical to the above left case
+		if z.parent == z.parent.parent.right {
+			y = z.parent.parent.left
+			if y.c == Red {
+				z.parent.c = Black
+				y.c = Black
+				z.parent.parent.c = Red
+				z = z.parent.parent
+				continue
+			}
+			if z == z.parent.left {
+				z = z.parent
+				t.rightRotate(z)
+			}
+			z.parent.c = Black
+			z.parent.parent.c = Red
+			t.leftRotate(z.parent.parent)
+			continue
+		}
+	}
+	t.root.c = Black
 }
