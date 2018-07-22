bd85c97951a3f83b5093967f16e2196ac0df246b
prantoran <prantoran@gmail.com>
2018-07-22 18:59:23 +0600

    bkp delete

--- rbt.go
+++ rbt.go
@@ -135,31 +135,47 @@ func (t *Tree) transplant(u, v *node) {
 	v.parent = u.parent
 }
 
-func (t *Tree) Delete(i Item) {
+func (t *Tree) Find(i Item) *node {
 	z := node{
 		key: i,
 	}
-	var y *node = &z
+	x := t.root
+	var y *node
+	for x != nil && z.key.Compare(x.key) {
+		y = x.left
+	}
+	if y.key == i {
+		return y
+	}
+	return nil
+}
+
+// Delete finds the node z for item i, deletes the item,
+// and fixes the chain of nodes upwards if
+// the properties are broken
+func (t *Tree) Delete(i Item) {
+	z := t.Find(i)
+	y := z
+	yc := y.c // y-original-color
 	var x *node
-	var yc = y.c // y-original-color
 	if z.left == nil {
 		x = z.right
-		t.transplant(&z, z.left)
+		t.transplant(z, z.right)
 	} else if z.right == nil {
 		x = z.left
-		t.transplant(&z, z.right)
+		t.transplant(z, z.left)
 	} else {
 		y = t.minimum(z.right)
 		yc = y.c
 		x = y.right
-		if y.parent == &z {
+		if y.parent == z {
 			x.parent = y
 		} else {
 			t.transplant(y, y.right)
 			y.right = z.right
 			y.right.parent = y
 		}
-		t.transplant(&z, y)
+		t.transplant(z, y)
 		y.left = z.left
 		y.left.parent = y
 		y.c = z.c
@@ -175,5 +191,34 @@ func (t *Tree) minimum(n *node) *node {
 }
 
 func (t *Tree) deleteFixup(x *node) {
-
+	for x != t.root && x.c == Black {
+		if x == x.parent.left {
+			w := x.parent.right
+			if w.c == Red { // case 1
+				w.c = Black
+				x.parent.c = Red
+				t.leftRotate(x.parent)
+				w = x.parent.right
+			}
+			if w.left.c == Black && w.right.c == Black { // case 2
+				w.c = Red
+				x = x.parent
+				continue
+			}
+			if w.right.c == Black { // case 3
+				w.left.c = Black
+				w.c = Red
+				t.rightRotate(w)
+				w = x.parent.right
+			}
+			// case 4
+			w.c = x.parent.c
+			x.parent.c = Black
+			w.right.c = Black
+			t.leftRotate(x.parent)
+			x = t.root
+			continue
+		}
+	}
+	x.c = Black
 }
