bd113ebcb95908c63a125539cfe960e896c355da
prantoran <prantoran@gmail.com>
2018-07-22 15:10:03 +0600

    init tree and leftrotate

--- rbt.go
+++ rbt.go
@@ -1,3 +1,25 @@
 package rbt
 
+// Tree is the Red-Black Tree
+type Tree struct {
+	root *node
+}
 
+// leftRotate rotates left by one node with x's position as pivot
+func (t *Tree) leftRotate(x *node) {
+	y := x.right
+	x.right = y.left
+	if y.left != nil {
+		y.left.parent = x
+	}
+	y.parent = x.parent
+	if x.parent == nil {
+		t.root = y
+	} else if x == x.parent.left {
+		x.parent.left = y
+	} else {
+		x.parent.right = y
+	}
+	y.left = x
+	x.parent = y
+}
