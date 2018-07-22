e2ad5f7aad62cd7472c3f347c6aaf1e564822df2
prantoran <prantoran@gmail.com>
2018-07-22 15:16:54 +0600

    right rotate

--- rbt.go
+++ rbt.go
@@ -23,3 +23,24 @@ func (t *Tree) leftRotate(x *node) {
 	y.left = x
 	x.parent = y
 }
+
+// rightRotate rotates right by one node with x's position as pivot.
+// It is symmetrical to leftRotate
+func (t *Tree) rightRotate(x *node) {
+	y := x.left
+	x.left = y.right
+	if y.right != nil {
+		y.right.parent = x
+	}
+	y.parent = x.parent
+	if x.parent == nil {
+		t.root = y
+	} else if x == x.parent.left {
+		x.parent.left = y
+	} else {
+		x.parent.right = y
+	}
+	y.right = x
+	x.parent = y
+
+}
