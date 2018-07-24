d2a89e8ed0cbad021031e793b0a263ef82dd6b40
prantoran <prantoran@gmail.com>
2018-07-24 11:52:51 +0600

    sample implementation of Item interface change Item.Compare arg to interface{} create dummy complete and balanced tree for testing in-order print of keys of tree for testing

--- item.go
+++ item.go
@@ -1,10 +1,38 @@
 package rbt
 
+const (
+	eps = 1e-9
+)
+
 // Item is the interface that the items must
 // satisfy to store in the red-black tree.
 // The Compare function is used by rbt to compare
 // two pairs of items in the tree.
 type Item interface {
 	//Compare should return true if parent item is less than compared item
-	Compare(Item) bool
+	Compare(u interface{}) bool
+}
+
+type Int struct {
+	key int
+}
+
+func (i Int) Compare(u interface{}) bool {
+	return i.key < u.(Int).key
+}
+
+type Long struct {
+	key int64
+}
+
+func (l *Long) Compare(u interface{}) bool {
+	return l.key < u.(Long).key
+}
+
+type Float struct {
+	key float64
+}
+
+func (f *Float) Compare(u interface{}) bool {
+	return f.key+eps < u.(Float).key
 }
--- rbt_test.go
+++ rbt_test.go
@@ -1,1 +1,99 @@
+
 package rbt
+
+import (
+	"fmt"
+	"testing"
+)
+
+func DummyTree() *Tree {
+	/*
+			                ___7(B)
+			               |
+			     |--6(R)---|
+			     |         |___5(B)
+		         |
+			4(B)-|
+			     |          ___3(B)
+			     |         |
+			     |--2(R)---|
+					       |___1(B)
+
+	*/
+	t := Tree{}
+	t.root = &node{
+		left:   nil,
+		right:  nil,
+		parent: nil,
+		key:    Int{4},
+		c:      Black,
+	}
+
+	t.root.left = &node{
+		left:   nil,
+		right:  nil,
+		parent: t.root,
+		key:    Int{2},
+		c:      Red,
+	}
+
+	t.root.right = &node{
+		left:   nil,
+		right:  nil,
+		parent: t.root,
+		key:    Int{6},
+		c:      Red,
+	}
+
+	t.root.left.left = &node{
+		left:   nil,
+		right:  nil,
+		parent: t.root.left,
+		key:    Int{1},
+		c:      Black,
+	}
+
+	t.root.left.right = &node{
+		left:   nil,
+		right:  nil,
+		parent: t.root.left,
+		key:    Int{3},
+		c:      Black,
+	}
+
+	t.root.right.left = &node{
+		left:   nil,
+		right:  nil,
+		parent: t.root.right,
+		key:    Int{5},
+		c:      Black,
+	}
+
+	t.root.right.right = &node{
+		left:   nil,
+		right:  nil,
+		parent: t.root.right,
+		key:    Int{7},
+		c:      Black,
+	}
+	return &t
+}
+
+func print(u *node) string {
+	ret := "("
+	if u.left != nil {
+		ret += print(u.left)
+	}
+	ret += fmt.Sprintf("%v", u.key)
+	if u.right != nil {
+		ret += print(u.right)
+	}
+	ret += ")"
+	return ret
+}
+
+func TestLeftRotate(t *testing.T) {
+	tree := DummyTree()
+	s := print(tree.root)
+	t.Logf("tree: %v", s)
+}
